cc9c15e02a91d37103b42672eda45d58ee1f406c
dev-zad <161847590+dev-zad@users.noreply.github.com>
2024-06-12 03:19:32 +0800

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    email: {
+        provider: 'strapi-provider-email-strapi-cloud',
+        providerOptions: {},
+        settings: {
+            defaultFrom: 'noreply@example.com',
+            defaultReplyTo: 'noreply@example.com',
+        },
+    },
+});
